eeeba93d6fc5eb81a01cb2030d443e7d3cf1385e
Olena Voroshnina <yuriyilena67@gmail.com>
2022-12-31 00:55:38 +0100

    add script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+
+
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+
+
+
+const makeGalleryItem = ({preview, original, description} = {}) => {
+    return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src='${preview}'
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+};
+
+const galleryItemsEl = galleryItems.map( (el) => {
+    return makeGalleryItem(el);
+}).join('');
+
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryItemsEl);
+
+
+
+
+galleryListEl.addEventListener('click', onLinkClick);
+
+function onLinkClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    };
+
+};
+
+new SimpleLightbox('.gallery .gallery__item', {captionsData: 'alt', captionDelay: 250});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+const contactFormEl = document.querySelector('.feedback-form');
+const userInfo = {};
+
+const fillContactFormFields = () => {
+    try {
+        const userInfoFromLS = JSON.parse(localStorage.getItem("feedback-form-state"));
+        
+        if (userInfoFromLS === null) {
+            return;
+        };
+
+        console.log(userInfoFromLS);
+
+        for (const prop in userInfoFromLS) {
+            contactFormEl.elements[prop].value = userInfoFromLS[prop];
+        }
+
+    } catch (error) {
+        console.log(error);
+    };
+
+};
+
+fillContactFormFields();
+
+const onContactFormInput = event => {
+    const { target } = event;
+
+    const fieldValue = target.value;
+    const fieldName = target.name;
+
+    userInfo[fieldName] = fieldValue;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(userInfo));
+
+};
+
+const onContactFormSubmit = event => {
+    event.preventDefault();
+
+    contactFormEl.reset();
+    localStorage.removeItem("feedback-form-state");
+};
+
+contactFormEl.addEventListener('input', onContactFormInput);
+contactFormEl.addEventListener('submit', onContactFormSubmit);
+
 
